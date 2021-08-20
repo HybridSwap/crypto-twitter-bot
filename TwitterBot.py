f4dc69249fe94ef1837e04f3f474e0835a208959
dinah <cybergirldinah@gmail.com>
2021-08-20 16:54:01 -0500

    initial commit

---
+++ TwitterBot.py
@@ -0,0 +1,43 @@
+"""
+    Author: Dinah Johnson
+
+"""
+
+from newsapi import NewsApiClient
+from api_key import my_api_key         
+import datetime
+import time
+import tweepy
+import schedule
+from credentials import *  
+
+todays_date = datetime.date.today()
+news_api = NewsApiClient(api_key=my_api_key) 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+def fetch():
+    try:
+        data = news_api.get_everything(q='cryptocurrency',from_param=todays_date,
+                                to=todays_date,language='en',
+                                sort_by='relevancy',
+                                page_size=40)
+        return data['articles']
+    except:
+        print("API error")
+    
+def getList():
+    articles = fetch()
+    links = []
+    for article in articles:
+        links.append(article['url'])
+    return links
+
+def job():
+    tweets = getList()
+    for tweet in tweets:
+        api.update_status(tweet)
+        time.sleep(1800)
+
+schedule.every().hour.at("08:00").do(job())
