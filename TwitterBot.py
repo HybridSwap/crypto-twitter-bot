618afd00027368a729692858e7eaaf9af4aa9f9a
dinah <cybergirldinah@gmail.com>
2021-08-20 19:12:49 -0500

    fixed config vars

--- TwitterBot.py
+++ TwitterBot.py
@@ -12,14 +12,14 @@ my_api_key = environ['my_api_key']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 ACCESS_TOKEN = environ['ACCESS_TOKEN']
 CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET =environ['CONSUMER_SECRET']
+CONSUMER_SECRET= environ['CONSUMER_SECRET']
 
 todays_date = datetime.date.today()
 news_api = NewsApiClient(api_key=my_api_key) 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
-
+ 
 def fetch():
     try:
         data = news_api.get_everything(q='cryptocurrency',from_param=todays_date,
