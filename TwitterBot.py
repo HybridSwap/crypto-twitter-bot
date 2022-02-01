d084ac07661bf3272b4c48c5e3f57273e11add70
HybridSwap <94022209+HybridSwap@users.noreply.github.com>
2022-02-01 13:52:29 +0100

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -9,21 +9,21 @@ import time
 import tweepy
 
 my_api_key = environ['my_api_key']
-ACCESS_SECRET = environ['ACCESS_SECRET']
-ACCESS_TOKEN = environ['ACCESS_TOKEN']
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET= environ['CONSUMER_SECRET']
+ACCESS_SECRET = environ['uWuVahlfOBP7PDSKcUVr786Awp8nMNTqXROO0gHeun40V']
+ACCESS_TOKEN = environ['1367218889860517894-7Y63vBnUNU0EQtv5imz38bssAFQNHP']
+CONSUMER_KEY = environ['jWQEyZIOgfJGT0CtYXGL1XnTS']
+CONSUMER_SECRET= environ['LjI8QanT9XBjrMT7rlK4racJmfu32n8mszQn0GvLStnATRuuZW']
 
 today = datetime.date.today()
 news_api = NewsApiClient(api_key=my_api_key) 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
-error_tweet = "Uh oh @Getinfotechent something went wrong! Please get me back up and running soon."
+error_tweet = "Oops @Getinfotechent something went wrong! Please get me back up and running soon."
  
 def fetch():
     try:
-        data = news_api.get_everything(q='cryptocurrency',from_param=today,
+        data = news_api.get_everything(q='binance',from_param=today,
                                 to=today,language='en',
                                 sort_by='relevancy',
                                 page_size=40)
